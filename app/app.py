11873fc1dfe62aac78e9b2bb3be7d1c0fc0be640
emoriiz <nico.kress1999@gmail.com>
2026-06-14 12:24:01 +0200

    change service to ClusterIP, final ci/cd test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hallo von der Flask Application</p>"
+    return "<p>Hallo von der Flask Application mit neuer Version</p>"
 
 @app.route("/healthz")
 def healthz():
